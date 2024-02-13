291f240cc2451885c89b423b05f42df8e8a838d3
Zahid Afsar <zahidtempz@icloud.com>
2024-02-13 10:52:11 -0800

    2ndcommit

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,5 @@
 let inputField = document.getElementById("inputField");
+let numInput = document.getElementById("numInput");
+let submitBtn = document.getElementById("submitBtn");
+let groupBtn = document.getElementById("groupBtn");
+let TotalNameCount = document.getElementById("TotalNameCount");
